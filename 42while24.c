94fd0e2d0d94c1a898730ebcde67ccd2db08cd27
Tara Keene <20keene@stfrancishs.org>
2018-09-18 17:22:59 +0000

    blastoff

--- 42while24.c
+++ 42while24.c
@@ -2,21 +2,22 @@
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
-    for (k=0;k<5;k++)
+    while (k<5)
+
     {
         sum+=k; //use something different in "while" version
+        printf ("%d\n",k);
+        k ++;
     }
+    printf ("Sum - %d\n",sum);
+    for (k=10; k>0; k--)
+    {
 
-    printf ("%d\n",sum);
+        printf ("%2d\n", k);
 
-    k=5;
-
-    while (k>0) {
-        k=k-1; //use something different in "for" version
-        printf ("%d\n", k);
     }
-
+    printf("Blastoff");
 }
 
